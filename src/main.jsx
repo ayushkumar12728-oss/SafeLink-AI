b26225261bf7a2ea6f06227d68270324bb5b6d2a
ayushkumar12728-oss <ayushkumar12728@gmail.com>
2026-07-30 21:40:26 +0530

    Fix production build

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { LanguageProvider } from "./context/LanguageContext";
-<LanguageProvider>
-    <App />
-</LanguageProvider>
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
